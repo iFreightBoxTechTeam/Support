e686da6f59c9cdd1318c04fdd18a256bde96c3f1
Intern1 <63729212+iFreightBoxTechTeam@users.noreply.github.com>
2025-05-23 15:58:57 +0530

    commited bye shreya

--- frontend/src/app/components/matable-list/matable-list.component.ts
+++ frontend/src/app/components/matable-list/matable-list.component.ts
@@ -39,6 +39,17 @@ export class MatableListComponent implements OnInit {
         this.matables = data;
       });
   }
+    // Method to update matable (you can trigger this on an "Edit" button, for example)
+  updateMatable(userId: number, updatedMatable: Matable): void {
+  console.log('Updating matable:', updatedMatable); // Log the data being sent
+  this.matableService.updateMatable(userId, updatedMatable).subscribe(
+    () => {
+      console.log('Matable updated successfully');
+      this.loadData();
+    },
+    (error) => console.error('Error updating matable:', error)
+  );
+}
    deleteMatable(userId: number): void {
     if (confirm('Are you sure you want to delete this item?')) {
       this.matableService.deleteMatable(userId).subscribe(
@@ -52,17 +63,7 @@ export class MatableListComponent implements OnInit {
     }
   }
 
-  // Method to update matable (you can trigger this on an "Edit" button, for example)
-  updateMatable(userId: number, updatedMatable: Matable): void {
-  console.log('Updating matable:', updatedMatable); // Log the data being sent
-  this.matableService.updateMatable(userId, updatedMatable).subscribe(
-    () => {
-      console.log('Matable updated successfully');
-      this.loadData();
-    },
-    (error) => console.error('Error updating matable:', error)
-  );
-}
+
 
   onSearch(term: string) {
     this.searchTerm = term;
