c0faf3b02b380a20b18bb56df97c3b0e3e77d392
intern3 <sidsolanki406@gmail.com>
2025-05-27 13:12:11 +0530

    comitted by Riddhi 27-5-25

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-showComponent(){
-  
-}
+export class HeaderComponent implements OnInit {
+  showAddButton: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // On route changes, check the URL
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.showAddButton = !this.router.url.includes('/statuslog');
+        console.log('Current route:', this.router.url, 'Show button:', this.showAddButton);
+      });
+
+    this.showAddButton = !this.router.url.includes('/statuslog');
+  }
+
+  showComponent() {
+    
+  }
 }
