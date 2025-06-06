797d42c086b4395898bf2b7b4865b2a8b763f165
intern3 <sidsolanki406@gmail.com>
2025-06-06 17:56:05 +0530

    commited by riddhi 6-6-25

--- sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
+++ sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
-export class AddUserComponent {
 
-}
+export class AddUserComponent {
+  
+}  
