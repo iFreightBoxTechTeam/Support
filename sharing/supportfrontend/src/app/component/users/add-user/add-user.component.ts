ac71c0d0810b665136f8bc26abe56c9a54722ea3
Intern2 <63729212+iFreightBoxTechTeam@users.noreply.github.com>
2025-06-18 15:33:04 +0530

    commited by aditya 18-06-25
    modal

--- sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
+++ sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
 
 declare var bootstrap: any;
 
@@ -8,6 +9,7 @@ declare var bootstrap: any;
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements AfterViewInit {
+
   @Output() userAdded = new EventEmitter<any>();
 
   @Output() userUpdated = new EventEmitter<any>();
@@ -32,17 +34,18 @@ export class AddUserComponent implements AfterViewInit {
 
  private nextId = 201;
 
- openModal(user?: any) {
-    if (user) {
-      this.isEditMode = true;
-      this.newUser = { ...user };
-    } else {
-      this.isEditMode = false;
-      this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
-    }
-    this.modalInstance?.show();
+openModal(user?: any) {
+  if (user) {
+    this.isEditMode = true;
+    this.newUser = { ...user };
+  } else {
+    this.isEditMode = false;
+    this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
   }
 
+  // Show the modal
+  this.modalInstance?.show();
+}
   addUser() {
     if (this.newUser.name.trim()) {
       if (this.isEditMode) {
@@ -59,5 +62,7 @@ export class AddUserComponent implements AfterViewInit {
 
   closeModal() {
     this.modalInstance?.hide();
-  }
-}
+ 
+
+} }
+
