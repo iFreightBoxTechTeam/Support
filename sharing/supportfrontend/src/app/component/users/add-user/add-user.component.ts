7d119fef6a675bf461f99a484752274853c774c7
Intern1 <63729212+iFreightBoxTechTeam@users.noreply.github.com>
2025-06-19 16:52:19 +0530

    COMMITED BY SHREYA 19-06-25

--- sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
+++ sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
@@ -1,76 +1,67 @@
-// import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 
-// declare var bootstrap: any;
+declare var bootstrap: any;
 
-// @Component({
-//   selector: 'app-add-user',
-//   templateUrl: './add-user.component.html',
-//   styleUrls: ['./add-user.component.css']
-// })
-// export class AddUserComponent implements AfterViewInit {
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements AfterViewInit {
 
-//   @Output() userAdded = new EventEmitter<any>();
+  @Output() userAdded = new EventEmitter<any>();
 
-//   @Output() userUpdated = new EventEmitter<any>();
+  @Output() userUpdated = new EventEmitter<any>();
 
-//   newUser = {
-//     id: 0,
-//     name: '',
-//     mobile: '',
-//     email: '',
-//     address: ''
-//   };
+  newUser = {
+    id: 0,
+    name: '',
+    mobile: '',
+    email: '',
+    address: ''
+  };
 
-//   modalInstance: any;
-//   isEditMode: boolean = false;
+  modalInstance: any;
+  isEditMode: boolean = false;
 
-//   ngAfterViewInit() {
-//     const modalElement = document.getElementById('addUserModal');
-//     if (modalElement) {
-//       this.modalInstance = new bootstrap.Modal(modalElement);
-//     }
-//   }
+  ngAfterViewInit() {
+    const modalElement = document.getElementById('addUserModal');
+    if (modalElement) {
+      this.modalInstance = new bootstrap.Modal(modalElement);
+    }
+  }
 
-//  private nextId = 201;
+ private nextId = 201;
 
-// openModal(user?: any) {
-//   if (user) {
-//     this.isEditMode = true;
-//     this.newUser = { ...user };
-//   } else {
-//     this.isEditMode = false;
-//     this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
-//   }
+openModal(user?: any) {
+  if (user) {
+    this.isEditMode = true;
+    this.newUser = { ...user };
+  } else {
+    this.isEditMode = false;
+    this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
+  }
 
   // Show the modal
-//   this.modalInstance?.show();
-// }
-//   addUser() {
-//     if (this.newUser.name.trim()) {
-//       if (this.isEditMode) {
-//         this.userUpdated.emit({ ...this.newUser });
-//       } else {
-//         this.userAdded.emit({ ...this.newUser });
-//       }
-//       this.modalInstance?.hide();
-//     } else {
-//       alert('Please enter a name.');
-//     }
-//   }
-
-
-//   closeModal() {
-//     this.modalInstance?.hide();
- 
-
-// } }
-
-
-
-
-
-// import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+  this.modalInstance?.show();
+}
+  addUser() {
+    if (this.newUser.name.trim()) {
+      if (this.isEditMode) {
+        this.userUpdated.emit({ ...this.newUser });
+      } else {
+        this.userAdded.emit({ ...this.newUser });
+      }
+      this.modalInstance?.hide();
+      this.loadUsers();
+    } else {
+      alert('Please enter a name.');
+    }
+  }
+  loadUsers() {
+    throw new Error('Method not implemented.');
+  }
 
 
 // declare var bootstrap: any;
@@ -147,4 +138,5 @@
 //     this.modalInstance?.hide();
  
 
-// } }
+} 
+
--- sharing/supportfrontend/src/app/component/users/users.component.ts
+++ sharing/supportfrontend/src/app/component/users/users.component.ts
@@ -126,127 +126,118 @@
 // }
 
 
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { AddUserComponent } from './add-user/add-user.component';
+import { User, UserService } from 'src/app/user.service';
 
 
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent implements OnInit {
+  @ViewChild('addUserComponent') addUserComponent!: AddUserComponent;
 
+  searchQuery: string = '';
+  nextId = 200;
 
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
 
+  users: User[] = [];
 
+  constructor(private userService: UserService) {}
 
+  ngOnInit() {
+    this.searchQuery = '';
+    this.loadUsers();
+  }
 
-
-// import { Component, ViewChild, OnInit } from '@angular/core';
-// import { AddUserComponent } from './add-user/add-user.component';
-// import { User, UserService } from 'src/app/user.service';
-
-
-// @Component({
-//   selector: 'app-users',
-//   templateUrl: './users.component.html',
-//   styleUrls: ['./users.component.css'],
-// })
-// export class UsersComponent implements OnInit {
-//   @ViewChild('addUserComponent') addUserComponent!: AddUserComponent;
-
-//   searchQuery: string = '';
-//   nextId = 200;
-
-//   currentPage: number = 1;
-//   itemsPerPage: number = 10;
-
-//   users: User[] = [];
-
-//   constructor(private userService: UserService) {}
-
-//   ngOnInit() {
-//     this.searchQuery = '';
-//     this.loadUsers();
-//   }
-
-//   loadUsers() {
-//     this.userService.getUsers().subscribe({
-//       next: (data) => {
-//         console.log("API Response:", data);
-//         this.users = data;
+  loadUsers() {
+    this.userService.getUsers().subscribe({
+      next: (data) => {
+        console.log("API Response:", data);
+        this.users = data;
           
-//   console.log("API Response:", data);
+  console.log("API Response:", data);
         
-//       },
-//       error: (error) => {
-//         console.error('Error fetching users:', error);
-//       }
-//     });
-//   }
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      }
+    });
+  }
 
-//   get filteredUsers(): User[] {
-//     return this.users.filter(user =>
-//       user.Name?.toLowerCase().includes(this.searchQuery.toLowerCase())
-//     );
-//   }
+  get filteredUsers(): User[] {
+    return this.users.filter(user =>
+      user.Name?.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
 
 
-//   get totalPages(): number {
-//     return Math.ceil(this.filteredUsers.length / this.itemsPerPage);
-//   }
+  get totalPages(): number {
+    return Math.ceil(this.filteredUsers.length / this.itemsPerPage);
+  }
 
-//   get paginatedUsers(): User[] {
-//     const start = (this.currentPage - 1) * this.itemsPerPage;
-//     return this.filteredUsers.slice(start, start + this.itemsPerPage);
-//   }
+  get paginatedUsers(): User[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredUsers.slice(start, start + this.itemsPerPage);
+  }
 
-//   changePage(page: number): void {
-//     if (page >= 1 && page <= this.totalPages) {
-//       this.currentPage = page;
-//     }
-//   }
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
 
-//   openAddUserModal() {
-//     this.addUserComponent.openModal();
-//   }
-//   closeAddUserModal(value: boolean) {
-//   }
-//   editUser(user: User) {
-//     this.addUserComponent.openModal(user);
-//   }
+  openAddUserModal() {
+    this.addUserComponent.openModal();
+  }
+  closeAddUserModal(value: boolean) {
+  }
+  editUser(user: User) {
+    this.addUserComponent.openModal(user);
+  }
 
-//   onUserUpdated(updatedUser: User) {
-//     this.userService.updateUser(updatedUser).subscribe({
-//       next: (user) => {
-//         const index = this.users.findIndex(u => u.issuesid === user.issuesid);
-//         if (index > -1) {
-//           this.users[index] = user;
-//         }
-//       },
-//       error: (err) => console.error('Error updating user:', err)
-//     });
-//   }
+  onUserUpdated(updatedUser: User) {
+    this.userService.updateUser(updatedUser).subscribe({
+      next: (user) => {
+        const index = this.users.findIndex(u => u.issuesid === user.issuesid);
+        if (index > -1) {
+          this.users[index] = user;
+        }
+      },
+      error: (err) => console.error('Error updating user:', err)
+    });
+  }
 
-//  onUserAdded(user: Omit<User, 'id'>) {
-//   if (user.Name.trim()) {
-//     this.userService.addUser(user).subscribe({
-//       next: (newUser) => {
-//         this.users.push(newUser);
-//         this.currentPage = this.totalPages;
-//       },
-//       error: (err) => console.error('Error adding user:', err)
-//     });
-//   }
-// }
+ onUserAdded(user: Omit<User, 'id'>) {
+  if (user.Name.trim()) {
+    this.userService.addUser(user).subscribe({
+      next: (newUser) => {
+        this.users.push(newUser);
+        this.currentPage = this.totalPages;
+      },
+      error: (err) => console.error('Error adding user:', err)
+    });
+  }
+}
 
-// deleteUser(id: number) {
-//   this.userService.deleteUser(id).subscribe({
-//     next: () => {
-//       this.users = this.users.filter(user => user.issuesid !== id);
-//     },
-//     error: err => console.error('Error deleting user:', err)
-//   });
-// }
+deleteUser(id: number) {
+  this.userService.deleteUser(id).subscribe({
+    next: () => {
+      this.users = this.users.filter(user => user.issuesid !== id);
+    },
+    error: err => console.error('Error deleting user:', err)
+  });
+}
 
-//   onSearchChange(newSearch: string) {
-//     this.searchQuery = newSearch;
-//     this.currentPage = 1;
-//     this.users = this.filteredUsers;
-//   }
-// }
+  onSearchChange(newSearch: string) {
+    this.searchQuery = newSearch;
+    this.currentPage = 1;
+    this.users = this.filteredUsers;
+  }
+}
 
 
