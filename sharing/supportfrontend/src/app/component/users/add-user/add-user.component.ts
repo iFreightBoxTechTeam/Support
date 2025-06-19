2f1ec1f47f185572f2c6502d78fbe5485ce159ea
Intern1 <63729212+iFreightBoxTechTeam@users.noreply.github.com>
2025-06-19 16:38:56 +0530

    commited by shreya 19-06-25

--- sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
+++ sharing/supportfrontend/src/app/component/users/add-user/add-user.component.ts
@@ -1,68 +1,150 @@
-import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+// import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 
-declare var bootstrap: any;
+// declare var bootstrap: any;
 
-@Component({
-  selector: 'app-add-user',
-  templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
-})
-export class AddUserComponent implements AfterViewInit {
+// @Component({
+//   selector: 'app-add-user',
+//   templateUrl: './add-user.component.html',
+//   styleUrls: ['./add-user.component.css']
+// })
+// export class AddUserComponent implements AfterViewInit {
 
-  @Output() userAdded = new EventEmitter<any>();
+//   @Output() userAdded = new EventEmitter<any>();
 
-  @Output() userUpdated = new EventEmitter<any>();
+//   @Output() userUpdated = new EventEmitter<any>();
 
-  newUser = {
-    id: 0,
-    name: '',
-    mobile: '',
-    email: '',
-    address: ''
-  };
+//   newUser = {
+//     id: 0,
+//     name: '',
+//     mobile: '',
+//     email: '',
+//     address: ''
+//   };
 
-  modalInstance: any;
-  isEditMode: boolean = false;
+//   modalInstance: any;
+//   isEditMode: boolean = false;
 
-  ngAfterViewInit() {
-    const modalElement = document.getElementById('addUserModal');
-    if (modalElement) {
-      this.modalInstance = new bootstrap.Modal(modalElement);
-    }
-  }
+//   ngAfterViewInit() {
+//     const modalElement = document.getElementById('addUserModal');
+//     if (modalElement) {
+//       this.modalInstance = new bootstrap.Modal(modalElement);
+//     }
+//   }
 
- private nextId = 201;
+//  private nextId = 201;
 
-openModal(user?: any) {
-  if (user) {
-    this.isEditMode = true;
-    this.newUser = { ...user };
-  } else {
-    this.isEditMode = false;
-    this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
-  }
+// openModal(user?: any) {
+//   if (user) {
+//     this.isEditMode = true;
+//     this.newUser = { ...user };
+//   } else {
+//     this.isEditMode = false;
+//     this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
+//   }
 
   // Show the modal
-  this.modalInstance?.show();
-}
-  addUser() {
-    if (this.newUser.name.trim()) {
-      if (this.isEditMode) {
-        this.userUpdated.emit({ ...this.newUser });
-      } else {
-        this.userAdded.emit({ ...this.newUser });
-      }
-      this.modalInstance?.hide();
-    } else {
-      alert('Please enter a name.');
-    }
-  }
+//   this.modalInstance?.show();
+// }
+//   addUser() {
+//     if (this.newUser.name.trim()) {
+//       if (this.isEditMode) {
+//         this.userUpdated.emit({ ...this.newUser });
+//       } else {
+//         this.userAdded.emit({ ...this.newUser });
+//       }
+//       this.modalInstance?.hide();
+//     } else {
+//       alert('Please enter a name.');
+//     }
+//   }
 
 
-  closeModal() {
-    this.modalInstance?.hide();
+//   closeModal() {
+//     this.modalInstance?.hide();
  
 
-} }
+// } }
 
+
+
+
+
+// import { Component, Output, EventEmitter, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+
+
+// declare var bootstrap: any;
+
+// @Component({
+//   selector: 'app-add-user',
+//   templateUrl: './add-user.component.html',
+//   styleUrls: ['./add-user.component.css']
+// })
+// export class AddUserComponent implements AfterViewInit {
+
+//   @Output() userAdded = new EventEmitter<any>();
+
+//   @Output() userUpdated = new EventEmitter<any>();
+
+//   newUser = {
+//     id: 0,
+//     name: '',
+//     mobile: '',
+//     email: '',
+//     address: ''
+//   };
+//  @Input()  editVal: any;
+
+//   ngOnInit(){
+//     if(this.isEditMode){
+//       this.newUser.name = this.editVal.name;
+//       this.newUser.mobile = this.editVal.mobile;
+//       this.newUser.email = this.editVal.email;
+//       this.newUser.address = this.editVal.address;
+//     }
+//   }
+
+//   @Output() removeModal:EventEmitter<boolean> = new EventEmitter<boolean>()
+
+//   modalInstance: any;
+//   @Input() isEditMode:any;
+
+//   ngAfterViewInit() {
+//     const modalElement = document.getElementById('addUserModal');
+//     if (modalElement) {
+//       this.modalInstance = new bootstrap.Modal(modalElement);
+//     }
+//   }
+
+//  private nextId = 201;
+
+// openModal(user?: any) {
+//   if (user) {
+//     this.isEditMode = true;
+//     this.newUser = { ...user };
+//   } else {
+//     this.isEditMode = false;
+//     this.newUser = { id: this.nextId++, name: '', mobile: '', email: '', address: '' };
+//   }
+
+//     this.modalInstance?.show();
+// }
+//   addUser() {
+//     if (this.newUser.name.trim()) {
+//       if (this.isEditMode) {
+//         this.userUpdated.emit({ ...this.newUser });
+//       } else {
+//         this.userAdded.emit({ ...this.newUser });
+//       }
+//       this.modalInstance?.hide();
+//     } else {
+//       alert('Please enter a name.');
+//     }
+//   }
+
+
+//   closeModal() {
+//     this.modalInstance?.hide();
+ 
+
+// } }
