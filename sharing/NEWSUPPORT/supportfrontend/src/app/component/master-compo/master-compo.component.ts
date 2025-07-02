a803fb9ca4c4de836feb60b0a3b59f7873eae270
intern3 <sidsolanki406@gmail.com>
2025-07-02 18:03:02 +0530

    commited by Riddhi 2-7-25

--- sharing/NEWSUPPORT/supportfrontend/src/app/component/master-compo/master-compo.component.ts
+++ sharing/NEWSUPPORT/supportfrontend/src/app/component/master-compo/master-compo.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, Output, EventEmitter, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-master-compo',
   templateUrl: './master-compo.component.html',
   styleUrls: ['./master-compo.component.css']
 })
-export class MasterCompoComponent {
 
+export class MasterCompoComponent implements AfterViewInit {
+  @Input() popup = false;
+  @Output() clickOutside = new EventEmitter<void>();
+  @ViewChild('popupRef') popupRef!: ElementRef;
+  panelStyles: any = {};
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const trigger = document.getElementById('settings-trigger');
+      if (trigger) {
+        const rect = trigger.getBoundingClientRect();
+        this.panelStyles = {
+          position: 'fixed',
+          top: `${rect.bottom + 10}px`,
+          left: `${rect.right - 320}px`
+        };
+      }
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const clickedInside = this.popupRef?.nativeElement.contains(event.target);
+    const gearClicked = (event.target as HTMLElement).closest('#settings-trigger');
+    if (!clickedInside && !gearClicked) {
+      this.clickOutside.emit();
+    }
+  }
+
+  hidePanel() {
+    this.clickOutside.emit();
+  }
 }
