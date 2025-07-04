125dee710eca1cb52c97677b75becf6fce76e0c7
Intern1 <63729212+iFreightBoxTechTeam@users.noreply.github.com>
2025-07-04 16:59:48 +0530

    commited be shreya 04-07-25

--- sharing/NEWSUPPORT/supportfrontend/src/app/issuse/issuse.component.ts
+++ sharing/NEWSUPPORT/supportfrontend/src/app/issuse/issuse.component.ts
@@ -56,10 +56,9 @@ loadIssues() {
   this.issueService.getIssues(this.searchTerm, this.currentPage, this.itemsPerPage).subscribe(data => {
     console.log(data)
     if (Array.isArray(data)) {
+      console.log("API did not return an array:", data);
       this.issues = data;
-      this.filteredIssues = data; // ✅ show full list initially
-    } else {
-      console.error("API did not return an array:", data);
+      this.filteredIssues = data; 
     }
   });
 }
